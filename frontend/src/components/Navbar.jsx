c11f88c373580aa3f3114e6adb43f07b690288d5
Aastha <aastha.2428cseai1398@kiet.edu>
2026-05-18 12:09:12 +0530

    final commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,49 +1,31 @@
 import { Link, useNavigate } from "react-router-dom";
-
 function Navbar() {
-
   const navigate = useNavigate();
-
   const logout = () => {
-
     localStorage.removeItem("token");
-
     navigate("/");
-
   };
-
   return (
-
     <nav className="navbar">
-
       <h2>AI Employee System</h2>
-
       <div>
-
         <Link to="/dashboard">
           Dashboard
         </Link>
-
         <Link to="/add-employee">
           Add Employee
         </Link>
-
         <Link to="/search">
           Search
         </Link>
-
         <Link to="/ai">
           AI
         </Link>
-
         <button onClick={logout}>
           Logout
         </button>
-
       </div>
-
     </nav>
   );
 }
-
 export default Navbar;
